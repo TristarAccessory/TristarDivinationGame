14d126f03c3bb58a0e7fc190374a51288ea352e4
whats2000 <whats2000mc@gmail.com>
2024-02-17 14:10:50 +0800

    修改: 修改背景樣式

--- divination-game/src/App.tsx
+++ divination-game/src/App.tsx
@@ -17,6 +17,7 @@ const Background = styled.div`
     left: 0;
     width: 100%;
     height: 100%;
+    max-height: 100vh;
     background-image: url(${background});
     background-size: cover;
     background-position: center;
